12ea70d355182d218713e99858514174bb516c51
tai <taibvps37458@fpt.edu.vn>
2024-11-25 10:12:57 +0700

    fist commit

---
+++ src/main/java/org/example/Caculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Caculator {
+
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    public Integer sub(Integer a, Integer b) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/ICaculator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface ICaculator {
+
+    Integer sum(Integer a, Integer b);
+    Integer sub(Integer a, Integer b);
+}
